aeb46d6539dd283f64af77d4ddb6bea5c0dcaa2c
PuntoAtoon <aarondh.10@gmail.com>
2026-04-15 18:22:21 -0500

    aaron
    acepta

--- src/main/java/com/mycompany/sistemagym/proyecto/vista/SistemaGymProyecto.java
+++ src/main/java/com/mycompany/sistemagym/proyecto/vista/SistemaGymProyecto.java
@@ -15,5 +15,7 @@ public class SistemaGymProyecto {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         scanner.close();
+        
+        //PARANGARICUTIRIMICUARO
     }
 }
