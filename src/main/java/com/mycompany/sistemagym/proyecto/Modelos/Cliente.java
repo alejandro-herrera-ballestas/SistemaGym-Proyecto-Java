93996d54dd07d059939d100f2879809f9c1c1fa9
PuntoAtoon <aarondh.10@gmail.com>
2026-05-07 17:35:14 -0500

    suma
    atributos y tales de los principales

--- src/main/java/com/mycompany/sistemagym/proyecto/Modelos/Cliente.java
+++ src/main/java/com/mycompany/sistemagym/proyecto/Modelos/Cliente.java
@@ -1,108 +1,105 @@
 package com.mycompany.sistemagym.proyecto.Modelos;
 
+import java.util.ArrayList;
+
 public class Cliente extends Usuario {
     private Plan planContratado;
     private float peso;
     private float altura;
     private boolean estado;
+    private ArrayList <Invitado> invitados;
 
     public Cliente(){}
 
-    public Cliente(String nombre, int edad, int id, String eps,String sexo, int telefono, String correo, Plan planContratado, float peso, float altura, boolean estado) {
+    public Cliente(Plan planContratado, float peso, float altura, boolean estado, ArrayList<Invitado> invitados, String nombre, int edad, int id, String eps, String sexo, int telefono, String correo) {
         super(nombre, edad, id, eps, sexo, telefono, correo);
         this.planContratado = planContratado;
         this.peso = peso;
         this.altura = altura;
         this.estado = estado;
+        this.invitados = invitados;
     }
+
+    
 
     public float getPeso() {
         return peso;
-    }
-
-    public float getAltura() {
-        return altura;
-    }
-
-    public boolean isEstado() {
-        return estado;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public int getEdad() {
-        return edad;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public String getEps() {
-        return eps;
-    }
-
-    public String getSexo() {
-        return sexo;
-    }
-
-    public int getTelefono() {
-        return telefono;
-    }
-
-    public String getCorreo() {
-        return correo;
-    }
-
-    public void setPlanContratado(Plan planContratado) {
-        this.planContratado = planContratado;
     }
 
     public void setPeso(float peso) {
         this.peso = peso;
     }
 
+    public float getAltura() {
+        return altura;
+    }
+
     public void setAltura(float altura) {
         this.altura = altura;
+    }
+
+    public boolean isEstado() {
+        return estado;
     }
 
     public void setEstado(boolean estado) {
         this.estado = estado;
     }
 
+    public String getNombre() {
+        return nombre;
+    }
+
     public void setNombre(String nombre) {
         this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
     }
 
     public void setEdad(int edad) {
         this.edad = edad;
     }
 
+    public int getId() {
+        return id;
+    }
+
     public void setId(int id) {
         this.id = id;
+    }
+
+    public String getEps() {
+        return eps;
     }
 
     public void setEps(String eps) {
         this.eps = eps;
     }
 
+    public String getSexo() {
+        return sexo;
+    }
+
     public void setSexo(String sexo) {
         this.sexo = sexo;
+    }
+
+    public int getTelefono() {
+        return telefono;
     }
 
     public void setTelefono(int telefono) {
         this.telefono = telefono;
     }
 
-    public void setCorreo(String correo) {
-        this.correo = correo;
+    public String getCorreo() {
+        return correo;
     }
 
-    @Override
-    public String toString() {
-        return "Cliente{" + "planContratado=" + planContratado + ", peso=" + peso + ", altura=" + altura + ", estado=" + estado + '}';
+    public void setCorreo(String correo) {
+        this.correo = correo;
     }
 
     
@@ -114,5 +111,17 @@ public class Cliente extends Usuario {
         return planContratado;
     }
 
+    public ArrayList<Invitado> getInvitados() {
+        return invitados;
+    }
+
+    public void setInvitados(Invitado invitados) {
+        this.invitados.add(invitados);
+    }
+
+    @Override
+    public String toString() {
+        return "Cliente{" + "planContratado=" + planContratado + ", peso=" + peso + ", altura=" + altura + ", estado=" + estado + ", invitados=" + invitados + '}';
+    }
 
 }
---
+++ src/main/java/com/mycompany/sistemagym/proyecto/Modelos/Invitado.java
@@ -0,0 +1,108 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.sistemagym.proyecto.Modelos;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class Invitado extends Usuario {
+    
+    private String fechaEntrada;
+    private boolean entradaHabilitada;
+
+    public Invitado() {
+    }
+
+    public Invitado(String fechaEntrada, boolean entradaHabilitada, String nombre, int edad, int id, String eps, String sexo, int telefono, String correo) {
+        super(nombre, edad, id, eps, sexo, telefono, correo);
+        this.fechaEntrada = fechaEntrada;
+        this.entradaHabilitada = entradaHabilitada;
+    }
+
+
+    public String getFechaEntrada() {
+        return fechaEntrada;
+    }
+
+    public void setFechaEntrada(String fechaEntrada) {
+        this.fechaEntrada = fechaEntrada;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getEps() {
+        return eps;
+    }
+
+    public void setEps(String eps) {
+        this.eps = eps;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public int getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public boolean isEntradaHabilitada() {
+        return entradaHabilitada;
+    }
+
+    public void setEntradaHabilitada(boolean habilitarEntrada) {
+        this.entradaHabilitada = habilitarEntrada;
+    }
+    
+    public void calculoDiasCumplidos(){ //Contar los dias, si son mas de 90 dias desde que entró de invitado, puede volver
+                                           // a entrar, si es menos, no se puede habilitar la entrada, puse void para que 
+                                           //no este rojo pero es boolean
+        
+        
+        
+    }
+    
+    
+    
+}
